6b9bc012829cf6d901415bae7c61ecf69c545402
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-22 10:28:31 -0400

    implemented product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,40 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(12,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+        isNonNegative(value) {
+          if (value < 0) {
+            throw new Error("Stock Cannot Be Negative");
+          }
+        }
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   },
   {
     sequelize,
