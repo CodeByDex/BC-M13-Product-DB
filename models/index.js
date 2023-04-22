09a5f36029f3098ead3c7ab32c860226c47c131a
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-22 11:10:46 -0400

    added product category relationship

--- models/index.js
+++ models/index.js
@@ -4,9 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "RESTRICT"
+})
 
 // Products belongToMany Tags (through ProductTag)
 
